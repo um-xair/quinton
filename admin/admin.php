cc9af04fc858ed12c4a6188bb5dd8456049f5cb3
Alyssa <natasyahebat@gmail.com>
2024-12-31 09:35:32 +0800

    31-12-24

--- admin/admin.php
+++ admin/admin.php
@@ -1,62 +1,76 @@
 <?php include 'sidebar.php'; ?>
         
-        <div class="p-6">
-            <h2 class="text-2xl text-black mb-4 font-bold">Welcome to the Dashboard, Xair!</h2>
-            <p class="text-gray-400 mb-6">Here you can manage your data, view reports, and more.</p>
-            
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                <div class="bg-[#FFFFFF] rounded-[30px] p-6 flex flex-col items-start">
-                    <div class="w-16 h-16 flex items-center justify-center bg-[#624FF6] rounded-full mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF">
-                            <path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/>
-                        </svg>
-                    </div>
-                    <h3 class="text-md text-black mb-2">Total Users</h3>
-                    <p class="text-4xl font-semibold text-black">150</p>
+<div class="p-6">
+    <h2 class="text-2xl text-black mb-4 font-bold">Welcome to the Quinton Baby, Xair!</h2>
+    <p class="text-gray-400 mb-6">Here you can manage your data, view reports, and more.</p>
+
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div class="grid grid-cols-2 gap-6">
+            <div class="bg-[#081225] rounded-[30px] p-6 flex flex-col items-start">
+                <div class="w-16 h-16 flex items-center justify-center bg-[#FFFFFF] rounded-full mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#081225">
+                        <path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/>
+                    </svg>
                 </div>
-    
-                <div class="bg-[#FFFFFF] rounded-[30px] p-6 flex flex-col items-start">
-                    <div class="w-16 h-16 flex items-center justify-center bg-[#E45A37] rounded-full mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF">
-                            <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/>
-                        </svg>
-                    </div>
-                    <h3 class="text-md text-black mb-2">Total Products</h3>
-                    <p class="text-4xl font-semibold text-black">150</p>
-                </div>
-    
-                <div class="bg-[#FFFFFF] rounded-[30px] p-6 flex flex-col items-start">
-                    <div class="w-16 h-16 flex items-center justify-center bg-[#479BF2] rounded-full mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF">
-                            <path d="M200-280v-280h80v280h-80Zm240 0v-280h80v280h-80ZM80-120v-80h800v80H80Zm600-160v-280h80v280h-80ZM80-640v-80l400-200 400 200v80H80Zm178-80h444-444Zm0 0h444L480-830 258-720Z"/>
-                        </svg>
-                    </div>
-                    <h3 class="text-md text-black mb-2">Total Sales</h3>
-                    <p class="text-4xl font-semibold text-black">150</p>
-                </div>
-    
-                <div class="bg-[#FFFFFF] rounded-[30px] p-6 flex flex-col items-start">
-                    <div class="w-16 h-16 flex items-center justify-center bg-[#5EC872] rounded-full mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF">
-                            <path d="m691-150 139-138-42-42-97 95-39-39-42 43 81 81ZM240-600h480v-80H240v80ZM720-40q-83 0-141.5-58.5T520-240q0-83 58.5-141.5T720-440q83 0 141.5 58.5T920-240q0 83-58.5 141.5T720-40ZM120-80v-680q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v267q-19-9-39-15t-41-9v-243H200v562h243q5 31 15.5 59T486-86l-6 6-60-60-60 60-60-60-60 60-60-60Zm120-200h203q3-21 9-41t15-39H240v80Zm0-160h284q38-37 88.5-58.5T720-520H240v80Zm-40 242v-562 562Z"/>
-                        </svg>
-                    </div>
-                    <h3 class="text-md text-black mb-2">Total Orders in Progress</h3>
-                    <p class="text-4xl font-semibold text-black">150</p>
-                </div>
+                <h3 class="text-md text-white mb-2">Total Users</h3>
+                <p class="text-4xl font-semibold text-white">150</p>
             </div>
 
-            <div class="mt-6">
-                <div class="relative flex flex-col rounded-[30px] bg-white bg-clip-border text-gray-700 h-[39vh]">
-                    <div class="relative mx-4 mt-4 flex flex-col gap-4 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none md:flex-row md:items-center"></div>
-                    <div class="pt-6 px-2 pb-0">
-                        <div id="bar-chart"></div>
-                    </div>
+            <div class="bg-[#081225] rounded-[30px] p-6 flex flex-col items-start">
+                <div class="w-16 h-16 flex items-center justify-center bg-[#FFFFFF] rounded-full mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#081225">
+                        <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/>
+                    </svg>
                 </div>
+                <h3 class="text-md text-white mb-2">Total Products</h3>
+                <p class="text-4xl font-semibold text-white">150</p>
             </div>
-            
+
+            <div class="bg-[#081225] rounded-[30px] p-6 flex flex-col items-start">
+                <div class="w-16 h-16 flex items-center justify-center bg-[#FFFFFF] rounded-full mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#081225">
+                        <path d="M200-280v-280h80v280h-80Zm240 0v-280h80v280h-80ZM80-120v-80h800v80H80Zm600-160v-280h80v280h-80ZM80-640v-80l400-200 400 200v80H80Zm178-80h444-444Zm0 0h444L480-830 258-720Z"/>
+                    </svg>
+                </div>
+                <h3 class="text-md text-white mb-2">Total Sales</h3>
+                <p class="text-4xl font-semibold text-white">150</p>
+            </div>
+
+            <div class="bg-[#081225] rounded-[30px] p-6 flex flex-col items-start">
+                <div class="w-16 h-16 flex items-center justify-center bg-[#FFFFFF] rounded-full mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#081225">
+                        <path d="m691-150 139-138-42-42-97 95-39-39-42 43 81 81ZM240-600h480v-80H240v80ZM720-40q-83 0-141.5-58.5T520-240q0-83 58.5-141.5T720-440q83 0 141.5 58.5T920-240q0 83-58.5 141.5T720-40ZM120-80v-680q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v267q-19-9-39-15t-41-9v-243H200v562h243q5 31 15.5 59T486-86l-6 6-60-60-60 60-60-60-60 60-60-60Zm120-200h203q3-21 9-41t15-39H240v80Zm0-160h284q38-37 88.5-58.5T720-520H240v80Zm-40 242v-562 562Z"/>
+                    </svg>
+                </div>
+                <h3 class="text-md text-white mb-2">Total Orders in Progress</h3>
+                <p class="text-4xl font-semibold text-white">150</p>
+            </div>
         </div>
+
+        <div class="bg-[#081225] rounded-[30px] p-6 flex flex-col items-center relative">
+    <!-- Circle Icon at Top Right -->
+    <div class="absolute top-4 right-4 w-12 h-12 flex items-center justify-center bg-[#FFFFFF] rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#081225">
+            <path d="M720-40q-83 0-141.5-58.5T520-240q0-83 58.5-141.5T720-440q83 0 141.5 58.5T920-240q0 83-58.5 141.5T720-40ZM240-600h480v-80H240v80Zm480 240H120v-80h600v80Z" />
+        </svg>
     </div>
+    
+    <!-- Product Photo -->
+    <div class="w-32 h-32 mb-4">
+        <img src="../assets/stroller.png" alt="Product Photo" class="rounded-lg object-cover w-full h-full">
+    </div>
+    
+    <!-- Product Details -->
+    <h3 class="text-lg font-semibold text-white">Product Name</h3>
+    <p class="text-sm text-gray-300 mb-1">Top Sales</p>
+    <p class="text-xl font-semibold text-white">RM 250</p>
+</div>
+
+
+    </div>
+</div>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script>
@@ -80,7 +94,7 @@
             dataLabels: {
             enabled: false,
         },
-            colors: ["#020617"],
+            colors: ["#081225"],
             plotOptions: {
             bar: {
                 columnWidth: "40%",
@@ -96,7 +110,7 @@
                 },
             labels: {
                 style: {
-                    colors: "#616161",
+                    colors: "#081225",
                     fontSize: "12px",
                     fontFamily: "inherit",
                     fontWeight: 400,
@@ -120,7 +134,7 @@
             yaxis: {
                 labels: {
                     style: {
-                        colors: "#616161",
+                        colors: "#081225",
                         fontSize: "12px",
                         fontFamily: "inherit",
                         fontWeight: 400,
